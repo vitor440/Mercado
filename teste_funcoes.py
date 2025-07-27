aa9793de9764225de8dd06a8ca086b4114feb15d
vitor440 <vitor16.souzaoliver@gmail.com>
2025-07-27 09:19:45 -0400

    Update teste_funcoes.py

--- teste_funcoes.py
+++ teste_funcoes.py
@@ -18,20 +18,20 @@ class TestFuncoes(unittest.TestCase):
     def test_add_loja(self):
         
 
-        add_loja(self.loja, TestProduto.p)
+        add_loja(self.loja, TestProduto.p) # Adicionando produto 'p' na loja.
         self.assertListEqual(
             self.loja,
             [TestProduto.p]
         )
 
-        add_loja(self.loja, TestProduto.p2)
+        add_loja(self.loja, TestProduto.p2) # Adiconando produto 'p2' na loja.
 
         self.assertListEqual(
             self.loja,
             [TestProduto.p, TestProduto.p2]
             )
 
-        add_loja(self.loja, TestProduto.p)
+        add_loja(self.loja, TestProduto.p) # Adicionando produto repetido na loja.
 
         self.assertListEqual(
             self.loja,
@@ -39,19 +39,19 @@ class TestFuncoes(unittest.TestCase):
         )
 #========================================================================#
     def test_add_carrinho(self):
-        add_carrinho(self.carrinho, TestProduto.p)
+        add_carrinho(self.carrinho, TestProduto.p)  # Adicionando produto 'p' no carrinho vazio.
         self.assertListEqual(
             self.carrinho,
             [{TestProduto.p : 1}]
         )
 
-        add_carrinho(self.carrinho, TestProduto.p)
+        add_carrinho(self.carrinho, TestProduto.p) # Adicionando produto 'p' repetido no carrinho.
         self.assertListEqual(
             self.carrinho,
             [{TestProduto.p : 2}]
         )
 
-        add_carrinho(self.carrinho, TestProduto.p2)
+        add_carrinho(self.carrinho, TestProduto.p2) # Adicionando produto 'p2' no carrinho.
         self.assertListEqual(
             self.carrinho,
             [{TestProduto.p : 2}, {TestProduto.p2 : 1}]
